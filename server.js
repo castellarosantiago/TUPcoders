099a6b036ac51ee67ef29c21d88001107bcc95d1
Caste_14 <castellarosantiago@gmail.com>
2024-08-06 19:19:27 -0300

    backend final actualizado para uso de postgreSQL, y para deploy en render

--- server.js
+++ server.js
@@ -1,23 +1,57 @@
-// Importamos el módulo Express
+require('dotenv').config();
 const express = require('express');
-// Creamos una instancia de una aplicación Express
+const bodyParser = require('body-parser');
+const { Pool } = require('pg');
+const path = require('path');
+
 const app = express();
-// Definimos el puerto en el que nuestro servidor escuchará
 const port = process.env.PORT || 3000;
 
-// Servimos archivos estáticos desde la carpeta 'public'
-app.use(express.static('public'));
+// Configuración de la conexión a PostgreSQL
+const pool = new Pool({
+  host: process.env.PG_HOST,
+  user: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
+  database: process.env.PG_DATABASE,
+  port: 5432,
+});
 
-// Definimos una ruta para la API que retorna un mensaje simple
+// Crear tabla si no existe
+pool.query(`
+  CREATE TABLE IF NOT EXISTS emails (
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(255) NOT NULL
+  );
+`, (err) => {
+  if (err) {
+    console.error('Error creating table:', err);
+  } else {
+    console.log('Table created or already exists');
+  }
+});
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/api/message', (req, res) => {
   res.json({ message: '¡Hola desde private! Saludos, Ian, Paula, y Caste' });
 });
 
-//Esto define una ruta para la URL raíz (/) que envía el archivo index.html al cliente.
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + "/public/index.html")
+app.post('/api/contact', async (req, res) => {
+  const { email } = req.body;
+  try {
+    await pool.query('INSERT INTO emails (email) VALUES ($1)', [email]);
+    res.status(200).json({ message: 'Correo recibido' });
+  } catch (err) {
+    console.error('Error al guardar el correo:', err);
+    res.status(500).json({ message: 'Error al guardar el correo' });
+  }
 });
-// Iniciamos el servidor para que escuche en el puerto especificado
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 app.listen(port, () => {
   console.log(`Servidor escuchando en http://localhost:${port}`);
 });
